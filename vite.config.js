681cc2c3e1de1ba78803728a3068b872b5c53d7e
jauregi13 <jon.j1305@gmail.com>
2023-10-12 17:13:53 +0200

    cambios en route

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "docs"
-  }
+  base: "/modulo3",
+  plugins: [react()]
 })
