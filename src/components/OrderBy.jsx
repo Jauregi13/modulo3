aa4219d12cce11e1087ba046684562708b91e863
jauregi13 <jon.j1305@gmail.com>
2023-10-14 15:58:25 +0200

    ordenación de imagenes con la busqueda hecho

--- src/components/OrderBy.jsx
+++ src/components/OrderBy.jsx
@@ -1,13 +1,12 @@
 import { Box,Typography,IconButton } from "@mui/material"
-import { UnfoldMore } from "@mui/icons-material"
 
 
 
-export const OrderBy = ({title}) => {
+export const OrderBy = ({title, onClick,icon}) => {
     return (
         <Box sx={{display:'inline-block'}}>
             <Typography variant="h5Second">{title}</Typography>
-            <IconButton><UnfoldMore /></IconButton>
+            <IconButton onClick={onClick}>{icon}</IconButton>
         </Box>
     )
 }
